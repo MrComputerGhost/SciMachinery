ef2ee5e00c0dd58564096a9e7c87f332a703b1cd
sci4me <minecraftman7778@gmail.com>
2014-01-03 05:07:50 -0600

    this shall take time...

---
+++ src/main/java/com/sci/machinery/block/tube/route/RouteFinder.java
@@ -0,0 +1,31 @@
+package com.sci.machinery.block.tube.route;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+import com.sci.machinery.block.TileTube;
+import com.sci.machinery.core.BlockCoord;
+
+public class RouteFinder
+{
+	protected World world;
+	protected TileTube tube;
+
+	public RouteFinder(World world, TileTube tube)
+	{
+		this.world = world;
+		this.tube = tube;
+	}
+
+	public BlockCoord route()
+	{
+		Map<ForgeDirection, Integer> costs = new HashMap<ForgeDirection, Integer>();
+		
+		//TODO calculate costs
+		
+		//TODO routing.. :P
+		
+		return null;
+	}
+}

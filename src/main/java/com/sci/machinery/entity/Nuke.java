797cb528e4f0c7e0eec07d98d51c5e7a5823c5d9
sci4me <minecraftman7778@gmail.com>
2014-01-19 21:31:18 -0600

    nukes

--- src/main/java/com/sci/machinery/entity/Nuke.java
+++ src/main/java/com/sci/machinery/entity/Nuke.java
@@ -1,13 +1,14 @@
 package com.sci.machinery.entity;
 
+import com.sci.machinery.SciMachinery;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockTNT;
 import net.minecraft.world.World;
 
 public class Nuke
 {
 	private int x, y, z;
 	private World world;
-	private int tmr;
 	private int power;
 
 	public Nuke(World world, int x, int y, int z)
@@ -16,7 +17,7 @@ public class Nuke
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		this.power = 50;
+		this.power = 25;
 	}
 
 	public void explode()
@@ -25,32 +26,19 @@ public class Nuke
 
 		int radius = power + this.world.rand.nextInt(10);
 
-		for(int x = 0; x < radius; x++)
+		for(int x = -radius; x < radius; x++)
 		{
-			for(int y = 0; y < radius; y++)
+			for(int y = -radius; y < radius; y++)
 			{
-				for(int z = 0; z < radius; z++)
+				for(int z = -radius; z < radius; z++)
 				{
-					if(fv(x, y, z) <= (radius * radius))
-					{
-						setBlock((x + this.x), (y + this.y), (z + this.z), 0);
-						setBlock(-(x + this.x), (y + this.y), (z + this.z), 0);
-						setBlock((x + this.x), -(y + this.y), (z + this.z), 0);
-						setBlock((x + this.x), (y + this.y), -(z + this.z), 0);
-						setBlock(-(x + this.x), -(y + this.y), (z + this.z), 0);
-						setBlock(-(x + this.x), (y + this.y), -(z + this.z), 0);
-						setBlock(-(x + this.x), -(y + this.y), -(z + this.z), 0);
-					}
+					if((x * x + y * y + z * z <= radius * radius) || this.world.rand.nextInt(100) < 10)
+						setBlock(x + this.x, y + this.y, z + this.z, 0);
 				}
 			}
 		}
 
 		this.world.spawnParticle("largeexplode", this.x, this.y, this.z, 1.0D, 0.0D, 0.0D);
-	}
-
-	private int fv(int x, int y, int z)
-	{
-		return x * x + y * y + z * z;
 	}
 
 	private void setBlock(int x, int y, int z, int id)
@@ -60,22 +48,15 @@ public class Nuke
 		if(block == null || block.blockID == Block.bedrock.blockID)
 			return;
 
+		if(block.blockID == Block.tnt.blockID || block.blockID == SciMachinery.instance.nukeId)
+		{
+			((BlockTNT) block).primeTnt(this.world, this.x, this.y, this.z, 4, null);
+		}
+
 		if(block.canDropFromExplosion(null))
 		{
 			block.dropBlockAsItemWithChance(this.world, x, y, z, this.world.getBlockMetadata(x, y, z), 1.0F / this.power, 0);
 			block.onBlockExploded(this.world, x, y, z, null);
-		}
-
-		tmr++;
-		if(tmr == 5)
-		{
-			tmr = 0;
-			double d3 = this.world.rand.nextDouble();
-			double d4 = this.world.rand.nextDouble();
-			double d5 = this.world.rand.nextDouble();
-			this.world.spawnParticle("explode", (this.x * 1.0D) / 2.0D, (this.y * 1.0D) / 2.0D, (this.z * 1.0D) / 2.0D, d3, d4, d5);
-			this.world.spawnParticle("smoke", x, y, z, d3, d4, d5);
-			this.world.playSoundEffect(this.x, this.y, this.z, "random.explode", 4.0F, (1.0F + (this.world.rand.nextFloat() - this.world.rand.nextFloat()) * 0.2F) * 0.7F);
 		}
 	}
 }
